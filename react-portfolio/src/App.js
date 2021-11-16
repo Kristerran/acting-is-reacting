0804605c0e918db0026006fe2f3d2beb5eacbefe
Kris Baily <kristopherbaily@gmail.com>
2021-11-15 18:17:50 -0800

    Imported all pages

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,8 +1,6 @@
 //Import our reactApp
 import React from "react";
 //import our header, Nav and footer
-import Header from "./components/Header"
-import Footer from "./components/footer";
 // Import our container which will have our scripts deciding which components to render
 import Container from "./components/Container"
 
--- react-portfolio/src/components/Container.js
+++ react-portfolio/src/components/Container.js
@@ -1,25 +1,28 @@
 import React, { useState } from 'react';
 import Nav from './Nav';
-import Home from './pages/Home';
-import About from './pages/About';
-import Blog from './pages/Blog';
+import AboutMe from './pages/AboutMe';
 import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
 export default function Container() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
+    if (currentPage === 'aboutMe') {
+      return <AboutMe />;
     }
-    if (currentPage === 'About') {
-      return <About />;
+    if (currentPage === 'portfolio') {
+      return <Portfolio />
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === 'contact') {
+      return <Contact />;
     }
-    return <Contact />;
+    if (currentPage === 'resume') {
+      return <Resume />;
+    }
+    return <AboutMe />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
